ad5e6bca30f6e85dbdc4ebdcfe91ba5c81b2b547
Adrian-DT <adriandt_work@outlook.com>
2026-05-11 17:35:38 +0200

    Implementando elemento de referencia para el scroll mediante viewChild

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,30 +1,23 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { ListComponent } from "../../components/list/list.component";
 import { GifService } from '../../services/gifs.service';
 
-// const imageUrls: string[] = [
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-// ];
-
 @Component({
   selector: 'app-trending-page',
-  imports: [ListComponent],
+  // imports: [ListComponent],
   templateUrl: './trending-page.component.html',
 })
 export default class TrendingPageComponent {
 
   // Inyectamos el gifService para obtener la información de la petición get de la API.
   gifService = inject(GifService);
+
+  // Creamos una propiedad para obtener la información referenciada de un elemento del div, pasando como selector la referencia del html #groupDiv
+  scrollDivRef = viewChild<ElementRef>('groupDiv');
+
+  onScroll(event: Event) {
+    // Obtenemos el elemento del scrollDivRef
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+  }
 
 }
